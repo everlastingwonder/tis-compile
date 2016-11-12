e145162ea9439baf69365cee27db41c4e49257a4
Gus Wiedey <guswiedey@gmail.com>
2016-11-12 15:00:00 -0500

    Create compile.java

---
+++ compile.java
@@ -0,0 +1,7 @@
+public class compile {
+  public static void main(String[] args) {
+    System.out.println("This is just a placeholder for now.");
+    System.out.print("Input: ");
+    System.out.print(args[0]);
+  }
+}
